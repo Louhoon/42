c51503830367c20d11eba49a64aad3bdeb41b8f6
lvoisin- <louane.voisin@hotmail.com>
2022-10-12 22:17:52 +0200

    fd

--- libft/libft.h
+++ libft/libft.h
@@ -63,4 +63,10 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 
 void	ft_putchar_fd(char c, int fd);
 
+void	ft_putstr_fd(char *s, int fd);
+
+void	ft_putendl_fd(char *s, int fd);
+
+void	ft_putnbr_fd(int n, int fd);
+
 #endif
