e62f2cae7b72dcefc9eeb53c469895698350e6ca
lvoisin- <louane.voisin@hotmail.com>
2022-10-13 23:01:10 +0200

    modif atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: lvoisin- <lvoisin-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 21:39:09 by lvoisin-          #+#    #+#             */
-/*   Updated: 2022/10/13 22:38:11 by lvoisin-         ###   ########.fr       */
+/*   Updated: 2022/10/13 23:00:46 by lvoisin-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,25 @@ int	ft_atoi(const char *str)
 	while (str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
 		|| str[i] == '\f' || str[i] == '\r' || str[i] == ' ')
 	i++;
-	if (str[i] == '-' || str[i] == '+')
+	if (str[i] == '-')
 	{
-	sign = 1 - 2 * (str[i] == '-');
+	sign = -1;
 	i++;
 	}
+	else if (str[i] == '+')
+	i++;
 	while (str[i] && str[i] >= '0' && str[i] <= '9')
 	{
 		result = result * 10 + str[i] - 48;
 		i++;
 	}
+	if (sign == -1)
+		return (-result);
 	return (sign * result);
 }
 
 // int main(void)
 // {
-// 	printf("%d\n", ft_atoi("\n\n\n  -46"));
+// 	printf("%d\n", ft_atoi("-123"));
 // 	return (0);
 // }
