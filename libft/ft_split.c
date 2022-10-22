92cd749e3bd8c48d90317952e446ce206f138759
Louane <louane@DESKTOP-6DTKV70.localdomain>
2022-10-22 04:45:16 +0200

    modif split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -12,53 +12,69 @@
 
 #include "libft.h"
 
-static    int    counter(const char *str, char c)
+static    int    counte_string(char *str, char c, int *count)
 {
     int i;
-
-    i = 1;
-    while (*str != '\0')
-    {
-        if (*str == c)
-        i++;
-        str++;
-    }
-    return (i);
-}
-
-static    int    take_word(char **new_string, int size, const char *s, char c)
-{
-    const char *start;
-    const char *end;
     int len;
-    int i;
-    
+
+    len = ft_strlen(str);
+    *count = 0;
     i = 0;
-    start = s;
-    while (i < size)
+    while (i < len)
     {
-    end = ft_strchr(start, c);
-    len = end - start + 1;
-    new_string[i] = malloc(sizeof(s) * len);
-    ft_strlcpy(new_string[i], start, len);
-        i++;
-        start = end + 1;
+        while (i < len)
+        {
+            if (strchr(str[i], c) == NULL)
+                break ;
+            i++;
+        }
+        int old_i;
+        old_i = i;
+        while (i < len)
+        {
+            if (strchr(str[i], c) != NULL)
+                break ;
+            i++;
+        }
+        if (i > old_i)
+            *count = *count + 1;
     }
-    return (1);
+    return (*count);
 }
 
-char    **ft_split(char const *s, char c)
-{
-    char **new_string;
-    int strings;
+// static    int    take_word(char **new_string, int size, const char *s, char c)
+// {
+//     const char *start;
+//     const char *end;
+//     int len;
+//     int i;
+    
+//     i = 0;
+//     start = s;
+//     while (i < size)
+//     {
+//     end = ft_strchr(start, c);
+//     len = end - start + 1;
+//     new_string[i] = malloc(sizeof(s) * len);
+//     ft_strlcpy(new_string[i], start, len);
+//         i++;
+//         start = end + 1;
+//     }
+//     return (1);
+// }
 
-    strings = counter(s, c);
-    new_string = malloc(sizeof(s) * strings + 1);
-	if (!new_string)
-		return (NULL);
-    take_word(new_string, strings, s, c);
-    return (new_string);
-}
+// char    **ft_split(char const *s, char c)
+// {
+//     char **new_string;
+//     int strings;
+
+//     strings = counter(s, c);
+//     new_string = malloc(sizeof(s) * strings + 1);
+// 	if (!new_string)
+// 		return (NULL);
+//     take_word(new_string, strings, s, c);
+//     return (new_string);
+// }
 
 // int main()
 // {
