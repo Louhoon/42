e26c47002adc2e3c600f149da504b252424fd1c7
lvoisin- <louane.voisin@hotmail.com>
2022-10-31 15:13:17 +0100

    new split

---
+++ libft/ft_split.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_split.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lvoisin- <lvoisin-@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/10/31 13:56:31 by lvoisin-      #+#    #+#                 */
+/*   Updated: 2022/10/31 15:12:18 by lvoisin-      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static char	**free_tab(char **result)
+{
+	int	i;
+
+	if (!result)
+		return (NULL);
+	i = 0;
+	while (result[i])
+	{
+		free(result[i]);
+		result[i] = NULL;
+		i++;
+	}
+	free (result);
+	return (NULL);
+}
+
+static	int	counter(const char *str, char c)
+{
+	int	i;
+	int	count_word;
+
+	i = 0;
+	count_word = 0;
+	while (str[i] == c && str[i])
+		i++;
+	while (str[i])
+	{
+		if (str[i] == c && str[i])
+		{
+			count_word++;
+			while (str[i] == c && str[i])
+			i++;
+		}
+		if (str[i] != c && str[i])
+			i++;
+	}
+	if (str[i - 1] != c)
+			count_word++;
+	return (count_word);
+}
+
+static	const char	*store_words(char **dest, const char *src, char c)
+{
+	int	len;
+	int	i;
+
+	len = 0;
+	while (*src == c)
+		src++;
+	while (src[len] != '\0' && src[len] != c)
+		len++;
+	*dest = malloc(sizeof(char) * (len + 1));
+	if (!*dest)
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		(*dest)[i] = src[i];
+		i++;
+	}
+	(*dest)[i] = '\0';
+	src = src + len + 1;
+	return (src);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**new_strings;
+	int		strings;
+	int		i;
+
+	strings = counter(s, c);
+	new_strings = malloc(sizeof(char *) * (strings + 1));
+	if (!new_strings)
+	{
+		new_strings[strings] = NULL;
+		i = 0;
+		while (i < strings)
+		{
+			new_strings = store_words(new_strings + i, s, c);
+			i++;
+		}
+	}
+	return (new_strings);
+}
+
+int main()
+{
+	char s[] = "hello you";
+	char *result;
+	result = *ft_split(s, ' ');
+	printf("%s\n", result);
+}
