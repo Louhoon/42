a696eb0a6e1d38dac88399cdbf1add05909cd751
lvoisin- <louane.voisin@hotmail.com>
2022-11-07 20:08:25 +0100

    printf

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_printf.h                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lvoisin- <lvoisin-@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/11/07 17:32:42 by lvoisin-      #+#    #+#                 */
+/*   Updated: 2022/11/07 20:06:50 by lvoisin-      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF
+#define FT_PRINTF
+
+# include <stdarg.h>
+
+int ft_printf(const char *str, ...);
+
+#endif
---
+++ ft_printf/main.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lvoisin- <lvoisin-@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/11/07 19:21:15 by lvoisin-      #+#    #+#                 */
+/*   Updated: 2022/11/07 20:06:46 by lvoisin-      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%c\n");
+	printf("%s\n");
+	printf("%p\n");
+	printf("%d\n");
+	printf("%i\n");
+	printf("%u\n");
+	printf("%x\n");
+	printf("%X\n");
+	printf("%%\n");
+}
